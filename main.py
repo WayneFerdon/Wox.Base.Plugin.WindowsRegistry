4e4ca01c43edbcdf39e3fc0c28a31a6f02cca804
WayneFerdon <wayneferdon@hotmail.com>
2023-04-11 05:38:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # Author: WayneFerdon wayneferdon@hotmail.com
 # Date: 2023-04-02 11:57:59
 # LastEditors: WayneFerdon wayneferdon@hotmail.com
-# LastEditTime: 2023-04-09 21:35:02
-# FilePath: \undefinedc:\Users\WayneFerdon\AppData\Roaming\FlowLauncher\Plugins\Wox.Base.Plugin.WindowsRegistry\main.py
+# LastEditTime: 2023-04-11 05:35:54
+# FilePath: \FlowLauncher\Plugins\Wox.Base.Plugin.WindowsRegistry\main.py
 # ----------------------------------------------------------------
 # Copyright (c) 2023 by Wayne Ferdon Studio. All rights reserved.
 # Licensed to the .NET Foundation under one or more agreements.
@@ -12,13 +12,10 @@
 # ----------------------------------------------------------------
 
 import os, sys
-import json
 import ctypes
 from winreg import *
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from WoxPluginBase_Query import *
-
-ICON = './Images/Registry.png'
 
 def is_admin():
     '''
@@ -41,12 +38,13 @@ class WindowsRegedit(QueryPlugin):
         # 'HKEY_PERFORMANCE_DATA':HKEY_PERFORMANCE_DATA,
         # 'HKEY_DYN_DATA':HKEY_DYN_DATA
     }
+
     def query(self, query:str):
         splited = repr(query)[1:-1].split('\\\\')
         if splited.count('') > 1:
             return None
-        root = splited[0]
         results = list()
+        root = splited[0]
         if len(splited) <= 1:
             for rootKeyName in WindowsRegedit.REG_ROOTS:
                 if rootKeyName == '':
@@ -59,45 +57,55 @@ class WindowsRegedit(QueryPlugin):
         if root not in WindowsRegedit.REG_ROOTS.keys():
             return None
         subKey = r'\\'.join(splited[1:-1]).replace('\\\\','\\')
-        keyHandle = OpenKey(ConnectRegistry(None,WindowsRegedit.REG_ROOTS[root]), subKey, access=KEY_ALL_ACCESS)
-        count, _, _ = QueryInfoKey(keyHandle)
-        if count == 0:
-            results.append(self.getQueryResult(root, subKey))
-        last = splited[-1]
-        for i in range(count):
-            subKeyName = repr(EnumKey(keyHandle, i))
-            if last.lower() not in subKeyName.lower():
-                continue
-            if subKey == '':
-                subPath = "\\".join([subKeyName[1:-1]])
-            else:
-                subPath = "\\".join([subKey, subKeyName[1:-1]])
-            results.append(self.getQueryResult(root, subPath))
+        subPath = None
+        try:
+            keyHandle = OpenKey(ConnectRegistry(None,WindowsRegedit.REG_ROOTS[root]), subKey, access=KEY_ALL_ACCESS)
+            count, _, _ = QueryInfoKey(keyHandle)
+            if count == 0:
+                results.append(self.getQueryResult(root, subKey))
+            last = splited[-1]
+            for i in range(count):
+                subKeyName = repr(EnumKey(keyHandle, i))
+                if last.lower() not in subKeyName.lower():
+                    continue
+                if subKey == '':
+                    subPath = "\\".join([subKeyName[1:-1]])
+                else:
+                    subPath = "\\".join([subKey, subKeyName[1:-1]])
+                results.append(self.getQueryResult(root, subPath))
+        except Exception as e:
+            if type(e) is PermissionError:
+                return [self.getPermissionErrorResult(root, subPath if subPath else subKey, e)]
+            self.debug(traceback.format_exc())
+            return [self.getExceptionResult(traceback.format_exc())]
         return results
 
-    def getQueryResult(self, root:str, sub:str):
+    def getQueryResult(self, root:str, subPath:str):
         try:
             a = ConnectRegistry(None,WindowsRegedit.REG_ROOTS[root])
-            subKeyHandle = OpenKey(a, sub,access=KEY_ALL_ACCESS)
+            subKeyHandle = OpenKey(a, subPath,access=KEY_ALL_ACCESS)
             subCount, valueCount, _ = QueryInfoKey(subKeyHandle)
             full = root
-            if sub != '':
-                full = full + '\\' + sub
+            if subPath != '':
+                full = full + '\\' + subPath
             subtitle = 'Sub keys:' + str(subCount) +' - Values:' + str(valueCount)
-            return QueryResult(full,subtitle,ICON,full, LauncherAPI.ChangeQuery.name, False, Plugin.actionKeyword + ' ' + full + '\\',True).toDict()
-        except Exception as e:
-            return self.getExceptionResult(root, sub, e)
+            return QueryResult(full,subtitle,self.defaultIcon,full, LauncherAPI.ChangeQuery.name, False, Plugin.actionKeyword + ' ' + full + '\\',True).toDict()
+        except PermissionError as e:
+            return self.getPermissionErrorResult(root, subPath, e)
         
-    def getExceptionResult(self, root:str, sub:str, e:Exception):
-        full = root
-        if sub != '':
-            full = full + '\\' + sub
-        return QueryResult(full, str(e), ICON, full, None, False).toDict()
+    def getPermissionErrorResult(self, root:str, subPath:str, e:Exception):
+        fullPath = root
+        if subPath != '':
+            fullPath += '\\' + subPath
+        subtitle = str(e)
+        if not is_admin():
+            subtitle += "PS: Flow is not run as administrator, but still able to open in Regedit. Use: → or Shift+Enter"
+        return QueryResult(fullPath, subtitle, self.defaultIcon, fullPath, None, False).toDict()
 
     def context_menu(self, fullKey:str): 
         results = list()
-        results.append(QueryResult(fullKey,'Press Enter to Open in Regedit',ICON,'',WindowsRegedit.openInRegedit.__name__,False,fullKey).toDict())
-        results.append(self.getCopyDataResult('Path',fullKey,ICON))
+        results.append(QueryResult('Open in Regedit', f'Press Enter to Open {fullKey}',self.defaultIcon,'',WindowsRegedit.openInRegedit.__name__,False,fullKey).toDict())
+        results.append(self.getCopyDataResult('Path',fullKey,self.defaultIcon))
         return results
     
     def openInRegedit(self, fullKey:str):
